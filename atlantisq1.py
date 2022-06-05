59c0736bd43639c8a61e1650e9cb13d778876d4b
anilmacharla25 <89156249+anilmacharla25@users.noreply.github.com>
2022-06-05 18:13:36 +0530

    atlantis interview question 1 added
    1.	Write a program which takes two strings as input from the user (str1 and str2). This program should print two strings as output (op1 and op2).
    a.	Output 1 should contain all the characters which are present in str1 but not present in str2.
    b.	Output 2 should contain all the characters which are present in str2 but not present in str1.
    EX:
    	Str1	Str2	Op1	Op2
    Example 1	ABC	BC	A	<null>
    Example 2	BC	BANGALORE	C	ANGLORE

---
+++ atlantisq1.py
@@ -0,0 +1,21 @@
+str1=input("enter the first str:")
+str2=input("enter second num:")
+# for i in str1:
+#     if i in str2:
+#         pass
+#     else :
+#         print(i)
+import numpy as np
+list1=[]
+for i in str2:
+    if i in str1:
+        pass
+    else :
+        print(i)
+        list1.append(i)
+print(list1)
+
+
+
+
+print(np.unique(list1))
